ab8824d7016dfe9b3db7816e306f1b7de9b00906
rajectedguy <rajectedguy@gmail.com>
2026-02-02 22:31:14 +0530

    02-02-2026

---
+++ FEBRUARY/02-02-2026/Max Circular Subarray Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    
+    public static int maxCircularSum(int a[]) {
+        int min = 0, max = Integer.MIN_VALUE, total = 0, minSum = 0, maxSum = 0;
+        for(int ele : a) {
+            total += ele;
+            minSum += ele;
+            maxSum += ele;
+
+            min = Math.min(min, minSum);
+            if(minSum > 0) minSum = 0;
+            max = Math.max(max, maxSum);
+            if(maxSum < 0) maxSum = 0;
+        }
+        
+        if(max < 0) return max;
+        return Math.max(max, (total - min));
+    }
+
+}
