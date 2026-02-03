7ffe13a06fb27b8358a747b8f30a1877db9be9b6
rajectedguy <rajectedguy@gmail.com>
2026-02-03 23:04:34 +0530

    03-02-2026

---
+++ FEBRUARY/03-02-2026/Stock Buy and Sell – Max one Transaction Allowed.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // Code here
+        int n = prices.length;
+        int maxprofit = 0;
+        int minprice = prices[0];
+        for(int i = 0; i<n; i++){
+            minprice = Math.min(prices[i],minprice);
+            int currprofit = prices[i]- minprice;
+            maxprofit = Math.max(maxprofit,currprofit);
+        }
+        return maxprofit;
+    }
+}
