c7c502e939c5664fdcab42262bd7d8a5b8528472
rajectedguy <rajectedguy@gmail.com>
2026-02-14 22:43:15 +0530

    13-02-2026

---
+++ FEBRUARY/14-02-2026/The Painter's Partition Problem-II.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minTime (self, arr, k):
+        def ok(mid):
+            currSum, cnt = 0, 1
+            for ele in arr:
+                if currSum + ele > mid:
+                    currSum = ele
+                    cnt += 1
+                    if cnt > k:
+                        return False
+                else:
+                    currSum += ele
+            return cnt <= k
+            
+        l, h = max(arr), sum(arr)
+        res = max(arr)
+        while l <= h:
+            mid = l+(h-l)//2
+            if ok(mid):
+                res = mid
+                h = mid-1
+            else:
+                l = mid+1
+        return res
