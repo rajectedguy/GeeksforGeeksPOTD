975684cb85e8352f9a46442888c18f48804a4b39
rajectedguy <rajectedguy@gmail.com>
2026-02-19 21:31:25 +0530

    19-02-2026

---
+++ FEBRUARY/19-02-2026/Missing Element in Range.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ArrayList<Integer> missingRange(int[] arr, int low, int high) {
+        // code here
+    HashSet<Integer> set = new HashSet<>();
+    for (int num : arr){
+        set.add(num);
+    } 
+    ArrayList<Integer> res = new ArrayList<>();
+    for (int i = low; i <= high; i++) {
+        if (!set.contains(i)){
+          res.add(i);  
+        } 
+    }
+    return res;  
+    }
+}
