acdc467290cdab0f923e45bcd1c68d2fc8506ade
rajectedguy <rajectedguy@gmail.com>
2026-02-27 21:58:16 +0530

    27-02-2026

---
+++ FEBRUARY/27-02-2026/Number of submatrix have sum X.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countSquare(self, mat, x):
+        # code here
+        n = len(mat)
+        m = len(mat[0])
+        pre = [[0]*(m+1) for _ in range(n+1)]
+        for i in range(n):
+            for j in range(m):
+                pre[i+1][j+1] = mat[i][j]
+                pre[i+1][j+1] += pre[i+1][j]
+                pre[i+1][j+1] += pre[i][j+1]
+                pre[i+1][j+1]-=pre[i][j]
+        cnt=0
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                # print(pre[i][j],end=" ")
+                for ln in range(1,min(i,j)+1):
+                    curr = pre[i][j]
+                    curr-=pre[i-ln][j]
+                    curr-=pre[i][j-ln]
+                    curr+=pre[i-ln][j-ln]
+                    if curr==x:
+                        cnt+=1
+                    
+            # print()
+        return cnt
