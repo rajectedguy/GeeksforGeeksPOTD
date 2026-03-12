e7ea8665e7b3c35779c4553499a404e017fe0162
rajectedguy <rajectedguy@gmail.com>
2026-03-12 21:54:15 +0530

    12-03-2026

---
+++ MARCH/12-03-2026/Minimum K Consecutive Bit Flips.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int kBitFlips(int[] arr, int k) {
+        // code here
+        Queue<Integer> qu= new LinkedList<>();
+        int n=arr.length;
+        int f=0;
+        for(int i=0;i<n;i++){
+            if(!qu.isEmpty() && qu.peek()+k==i){
+                qu.poll();
+            }
+            int cr=arr[i];
+            if(qu.size()%2!=0){
+                cr^=1;
+            }
+            if(cr==0){
+                if(i+k>n){
+                    return -1;
+                }
+                qu.offer(i);
+                f++;
+            }
+        }
+        return f;
+    }
+}
