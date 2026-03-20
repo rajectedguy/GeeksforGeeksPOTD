5e4616b10bf3f95d79cc05ecb03e8e686bdb8ffb
rajectedguy <rajectedguy@gmail.com>
2026-03-20 21:52:08 +0530

    20-03-2026

---
+++ MARCH/20-03-2026/Predecessor and Successor.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ArrayList<Node> findPreSuc(Node root, int k) {
+        ArrayList<Node> res = new ArrayList<Node>();
+        res.add(null);
+        res.add(null);
+        Node node = root;
+        while(node != null) {
+            if(node.data < k) {
+                res.set(0, node);
+                node = node.right;
+            } 
+            else if(node.data > k) {
+                res.set(1, node);
+                node = node.left;
+            }
+            else {
+                Node temp = node.left;
+                while(temp != null && temp.right != null) {
+                    temp = temp.right;
+                }
+                if(temp != null) res.set(0, temp);
+                temp = node.right;
+                while(temp != null && temp.left != null) {
+                    temp = temp.left;
+                }
+                if(temp != null) res.set(1, temp);
+                break;
+            }
+        }
+        return res;
+    }
+}
