94cc57424c1cee4edddab3d0fb8e9331ec196d38
rajectedguy <rajectedguy@gmail.com>
2026-04-24 18:45:26 +0530

    24-04-2026

---
+++ 2026_GFG_POTD/APRIL/24-04-2026/Buildings with Sunlight.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int visibleBuildings(int arr[]) {
+        int ans = 1;
+        int first = arr[0];
+        for(int i = 1;i < arr.length;i++){
+            if(arr[i] >= first){
+                ans++;
+                first = arr[i];
+            }
+        }
+        return ans;
+    }
+}
