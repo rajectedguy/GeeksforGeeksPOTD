008aad0298c48bd1a3c6fb85c89f1242477c660d
rajectedguy <rajectedguy@gmail.com>
2026-05-21 18:47:36 +0530

    21-05-2026

---
+++ 2026_GFG_POTD/MAY/21-05-2026/Check if All Bits Set.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isBitSet(int n) {
+        if(n == 0) {
+            return false;
+        }
+        while(n > 0) {
+            if( (n&1) == 0) {
+                return false;
+            }
+            n >>= 1;
+        }
+        return true;
+    }
+};
