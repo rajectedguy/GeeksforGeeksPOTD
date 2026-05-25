9a8143e1fbf1647f8627227b1bf8f87ea17996c4
rajectedguy <rajectedguy@gmail.com>
2026-05-25 17:19:24 +0530

    25-05-2026

---
+++ 2026_GFG_POTD/MAY/25-05-2026/Elements in the Range.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkElements(int start, int end, int[] arr) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            set.add(arr[i]);
+        }
+        for(int i=start;i<=end;i++){
+            if(!set.contains(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
