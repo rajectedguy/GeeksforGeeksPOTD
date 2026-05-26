0d999c1757604e8c50b4c9909732caa30dd15bef
rajectedguy <rajectedguy@gmail.com>
2026-05-26 19:04:37 +0530

    26-05-2026

---
+++ 2026_GFG_POTD/MAY/26-05-2026/Minimum Toggle to Partition.java
@@ -0,0 +1,22 @@
+class Solution {
+    int minToggle(int[] arr) {
+        int n = arr.length;
+        int totalZeros = 0;
+        for (int num : arr) { 
+            if (num == 0) {
+                totalZeros++;
+            }
+        }
+        int currentToggles = totalZeros;
+        int minToggles = currentToggles;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                currentToggles++;
+            } else {
+                currentToggles--;
+            }
+            minToggles = Math.min(minToggles, currentToggles);
+        }
+        return minToggles;
+    }
+}
