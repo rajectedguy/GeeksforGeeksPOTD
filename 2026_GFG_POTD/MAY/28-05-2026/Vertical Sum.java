fc63409bb6acb05dc607cf1bd77b9b7093d9a0ff
rajectedguy <rajectedguy@gmail.com>
2026-05-28 14:56:54 +0530

    28-05-2026

---
+++ 2026_GFG_POTD/MAY/28-05-2026/Vertical Sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    TreeMap<Integer,Integer> map;
+    public ArrayList<Integer> verticalSum(Node root) {
+        map = new TreeMap<>();
+        calculate(0,root);
+        
+        return new ArrayList<>(map.values());
+    }
+    
+    public void calculate(int level, Node root){
+        if(root == null)
+            return;
+            
+        int previousVal = map.getOrDefault(level, 0);
+        map.put(level , previousVal + root.data);
+        
+        calculate(level - 1 , root.left);
+        calculate(level + 1 , root.right);
+    }
+}
