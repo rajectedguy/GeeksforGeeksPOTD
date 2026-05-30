1cad596dde02955495b50d9294ba64f5ae2377ca
rajectedguy <rajectedguy@gmail.com>
2026-05-30 21:05:14 +0530

    30-05-2026

---
+++ 2026_GFG_POTD/MAY/30-05-2026/Replace with XOR of Adjacent.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void replaceElements(int[] arr) {
+        int n = arr.length;
+        int first = arr[0] ^ arr[1];
+        int last = arr[n - 2] ^ arr[n - 1];
+        int prev = arr[0];
+        for (int i = 1; i < n - 1; i++) {
+            int curr = arr[i];
+            arr[i] = prev ^ arr[i + 1];    
+            prev = curr;
+        }
+        arr[0] = first;
+        arr[n - 1] = last;
+    }
+}
