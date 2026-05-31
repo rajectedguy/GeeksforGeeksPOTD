3b2b3fb7def028229395bf3d447c71e1985d4ced
rajectedguy <rajectedguy@gmail.com>
2026-05-31 18:10:58 +0530

    31-05-2026

---
+++ 2026_GFG_POTD/MAY/31-06-2026/Express as Consecutive Number Sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSumOfConsecutive(int n) {
+        if(n==1) return false;
+        while(n > 1){
+            if(n%2 !=0){
+                return true;
+            }
+            n=n/2;
+        }
+        return false;
+    }
+}
