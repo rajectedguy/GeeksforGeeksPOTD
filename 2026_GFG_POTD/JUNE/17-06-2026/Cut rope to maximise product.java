4ed6ce0b3a6d51344d7af20c8e31ea1ac313513c
rajectedguy <rajectedguy@gmail.com>
2026-06-17 06:10:47 +0530

    17-06-2026

--- 2026_GFG_POTD/JUNE/17-06-2026/Cut rope to maximise product.java
+++ 2026_GFG_POTD/JUNE/17-06-2026/Cut rope to maximise product.java
@@ -7,14 +7,12 @@ class Solution {
         if(n>=201){
             return 9214223067318710998L;
         }
-        
         long[] dp = new long[n+1];
         Arrays.fill(dp,-1);
         dp[0] = 0;
         dp[1] = 0;
         dp[2] = 1;
         dp[3] = 2;
-        
         for(int j=4;j<=n;j++){
             long max = 0;
             for(int i=1;i<j;i++){
